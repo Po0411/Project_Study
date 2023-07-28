142c1cfe26ea2a9b5dfd039d10cfa5a7a0ef7ee0
조하랑 <wawa111027@gmail.com>
2023-07-28 21:49:01 +0900

    Update Hello World.py

--- 언어/Python/Hello World.py
+++ 언어/Python/Hello World.py
@@ -1,5 +1,11 @@
 #해당 Hello World.py 는 여러 가지 출력을 해보는 걸 기본 목적으로 잡고 있습니다.
 
-print("Hello World") #print()로 Hello World출력하기
+#파이썬 오류 확인 방법
+#SyntaxError(문법 오류) : 파이썬의 문법, 즉 규칙에 맞지 않아서 컴퓨터가 해석 할 수 없을때 발생한다
+#NameErrir(이름 오류) : 사람이 컴퓨터에 미리 알려주지 않은 이름을 약속 없이 그냥 쓸때 발생한다 ex)변식자가 없는 경우
+#TypeErrir(데이터 종류 오류) : 데이터의 종류에 알맞지 않은 규칙으로 데이터를 처리하려고 할 때 발생한다
 
+
+#1.기본적인 파이썬 print() 명령어
+print("Hello World") #print()로 Hello World출력하기
 print("안녕하세요!\n저는 홍길동 입니다!") #\n 는 줄바꿈 기능 입니다.
